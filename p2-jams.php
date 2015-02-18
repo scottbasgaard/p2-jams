dd52eb2e78a7bdac4ec002cc912599a805c4b4e5
richard swain <richard@humanmade.co.uk>
2015-02-18 12:56:08 +0000

    remove var dump

--- p2-jams.php
+++ p2-jams.php
@@ -264,13 +264,3 @@ function P2_Jams() {
 
 // Global for backwards compatibility.
 $GLOBALS['p2_jams'] = P2_Jams();
-
-
-
-function var_error_log($object){
-    ob_start();                    // start buffer capture
-    var_dump($object);             // dump the values
-    $contents = ob_get_contents(); // put the buffer into a variable
-    ob_end_clean();                // end capture
-    error_log($contents);          // log contents of the result of var_dump($object)
-}
